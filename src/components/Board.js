a73efb7e7540d581684a27c4ce6bab41010abbab
Ingela Löfgren <ingela.lofgren@gmail.com>
2020-11-16 09:52:04 +0100

    Made updates after watching codealong

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Square } from 'components/Square'
 
 export const Board = () => {
-  // TODO - use the 'squares' state from the game reducer
-  const squares = []
+  // ((store) function takes the entire store from redux and retirns something from that store (store.game.squares)
+  const squares = useSelector ((store) => (store.game.squares))
 
   return (
     <div className="board">
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { game } from 'reducers/game'
 import { Player } from 'components/Player'
 
 export const Square = ({ value, index }) => {
+  //dispatch allows us to trigger things within our store but we need an action to dipatch
+  const dispatch = useDispatch()
+
   const handleClick = () => {
-    // TODO send the captureSquare action
+    //Here we dispatch an action. Index is the payload with the value of whatever that is at the moment
+    dispatch(game.actions.captureSquare({ index }))
   }
 
   return (
